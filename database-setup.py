bd64eeb31b577e786cb901a29a135e28ccc323f8
Andrei Oliveira <andrei.coliv@gmail.com>
2020-01-08 15:46:51 -0300

    Modelo pro banco de dados armazenar as requisições adicionado

--- database-setup.py
+++ database-setup.py
@@ -6,6 +6,16 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class Book(Base):
+   __tablename__ = 'request'
+
+   id = Column(Integer, primary_key=True)
+   component = Column(String(64), nullable=False)
+   version = Column(String(64), nullable=False)
+   owner = Column(String(64), nullable=False)
+   status = Column(String(64), nullable=False)
+   date = Column(Date, default=_get_date)
+
 engine = create_engine('sqlite:///requests.db')
 
 Base.metadata.create_all(engine)
