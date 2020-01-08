0000368a042c9c93c6fa2a8331aafaf7db0cbc22
Andrei Oliveira <andrei.coliv@gmail.com>
2020-01-08 15:15:34 -0300

    Inserido o requerimento e setup do banco de dados para a aplicação

---
+++ database-setup.py
@@ -0,0 +1,11 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+engine = create_engine('sqlite:///requests.db')
+
+Base.metadata.create_all(engine)
