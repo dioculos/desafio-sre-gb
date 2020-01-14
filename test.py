c4e9b1c11a3a03f43cf5557f485cdea0dced67e0
Andrei Oliveira <andrei.coliv@gmail.com>
2020-01-13 22:47:44 -0300

    Incluindo testes positivos e negativos

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ class TestApp(unittest.TestCase):
     def test_connect(self):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
-        print('Connection OK')
+        print('\nConnection OK\n')
     def test_post(self):
         with self.app as client:
             sent = {'component':'testcomp','version':'1','owner':'testapp','status':'ok'}
@@ -29,5 +29,46 @@ class TestApp(unittest.TestCase):
                 '"owner":"testapp","status":"ok","version":"1"}}',
                 result.data
             )            
+            print('\nInitial Post OK\n')
+    def test_fail_component_missing(self):
+        with self.app as client:
+            result = client.post(
+                '/api',
+                data=json.dumps(dict(version='1', owner='testapp', status='ok')),
+                content_type='application/json'
+            )
+            self.assertIn('400', result.status)
+            self.assertIn('component is missing', result.data)
+            print('\nComponent Missing error OK\n')
+    def test_fail_version_missing(self):
+        with self.app as client:
+            result = client.post(
+                '/api',
+                data=json.dumps(dict(component='1', owner='testapp', status='ok')),
+                content_type='application/json'
+            )
+            self.assertIn('400', result.status)
+            self.assertIn('version is missing', result.data)
+            print('\nVersion Missing error OK\n')
+    def test_fail_owner_missing(self):
+        with self.app as client:
+            result = client.post(
+                '/api',
+                data=json.dumps(dict(component='1', version='testapp', status='ok')),
+                content_type='application/json'
+            )
+            self.assertIn('400', result.status)
+            self.assertIn('owner is missing', result.data)
+            print('\nOwner Missing error OK\n')
+    def test_fail_status_missing(self):
+        with self.app as client:
+            result = client.post(
+                '/api',
+                data=json.dumps(dict(component='1', version='testapp', owner='ok')),
+                content_type='application/json'
+            )
+            self.assertIn('400', result.status)
+            self.assertIn('status is missing', result.data)
+            print('\nStatus Missing error OK\n')
 if __name__ == '__main__':
     unittest.main()
